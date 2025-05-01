8226d098b4f560d94b4593bf1b1b46e8cedd6f13
09junaid <junaidarshad8686@gmail.com>
2025-05-01 12:40:31 +0500

    Userpreview changes

--- src/modules/preview/UserPreview.tsx
+++ src/modules/preview/UserPreview.tsx
@@ -158,13 +158,13 @@ export default function UserPreview() {
           </div>
               <div>
               <div className=" flex">
-              <button
+              <p
                 onClick={downloadAsPDF}
-                className=" transition px-6 py-2 rounded-lg shadow-md"
+                className=" transition px-6 py-2 cursor-pointer rounded-lg shadow-md"
                 style={{ backgroundColor: '#0072FF', color: 'white' }}
               >
                 Download as PDF
-              </button>
+              </p>
             </div>
               </div>
           {/* Uploaded Images */}
@@ -180,7 +180,7 @@ export default function UserPreview() {
               >
                 Your Package Includes
               </span>
-              <div className="mt-4 grid grid-cols-2 md:grid-cols-2 gap-4">
+              <div className="mt-8 grid grid-cols-2 md:grid-cols-2 gap-4">
                 {formData.images.map((img, idx) => (
                   <img
                     key={idx}
